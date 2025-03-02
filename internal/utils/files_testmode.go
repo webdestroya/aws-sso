86614107e0b5f3b6625f0fe8c5e5a3249758a745
webdestroya <webdestroya@users.noreply.github.com>
2025-03-01 23:42:25 -0800

    test fixes

--- internal/utils/files_testmode.go
+++ internal/utils/files_testmode.go
@@ -5,9 +5,9 @@ package utils
 import "os"
 
 func AtomicWriteFile(filename string, data []byte, fileMode os.FileMode) error {
-
+	return nil
 }
 
 func WriteFile(filename string, data []byte, fileMode os.FileMode) error {
-
+	return nil
 }
